0e26b2754a66b8ecd2fe0c6b639af3a7d0381869
Matías Araujo <matiasnahuel.-@hotmail.com>
2022-04-25 18:31:48 -0300

    There are an error in the contract, but the logic seems to be fine

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # sushiswap-challenge
 Challenge for Membrane: An interaction with SushiSwap.
+
+## Instructions
+Not tried in Linux, at now, only works in Windows, but I'think that this will work too in Linux.
+
+### Installation
+> yarn install
+
+Please don't forget to create a .env file with the following data:
+
+>INFURA_KEY="YOUR_INFURA_KEY"
+>
+>MNEMONIC="please do not use your real seed phrase here"
+>
+>ETHERSCAN_API_KEY="ETHERSCAN_API_KEY_FOR_CODE_VERIFICATION"
+>
+>MAIN_ACCOUNT="YOUR_ACCOUNT_FOR_DEPLOYMENT"
+
+### Compile
+> npx hardhat compile
+
+### Deploy in Testnet
+First, edit _./scripts/deploy_in_testnet.ts_ with the address of SushiSwap in the desired network. At now, it works in Kovan Testnet Network.
+
+> npx hardhat run --network kovan .\scripts\deploy_in_testnet.ts --verbose
+
+### Verify
+> npx hardhat verify {ADDR_FROM_LAST_STEP} --network kovan  --verbose
+
+### Usage
+With the Deployment Address, transfer the desired amount of two testnet tokens to the contract and call the function **_subscribeToPool_** in Etherscan.
+It will call _approve_ for the desired tokens (Please take note of the address of your tokens) and their amounts.
+Afterwards, it will call SushiSwap router for the desired pair, with a slippage of 0.5%.
+Lastly, it will approve SLP tokens for the contract.
+
+**KNOWN LIMITATION**: 
+- At now, it will not put SLP tokens in Yield Farming. You can rescue the SLP tokens from the contract, and do it by yourself. Honestly, at the time being, I couldn't find the function in Sushi to do this in an automated way. (The prototype of the functions are now in the code, but does nothing. I think that is _deposit_ in MasterChef, but I'm not sure)
+- Only support pair of tokens, do not support ETH-Another token. (Only implies modify the caller function, with payable and return the dust ETH not used in transaction, but it is not implemented at now)
+- The contract try to remove liquidity, but it can crash.
+- Tests? where we're going we don't need *tests* (Not really, I owe you the tests)
+- It could be improved with Openzeppelin upgradable plugins.
--- contracts/SushiWallet.sol
+++ contracts/SushiWallet.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 // >0.8 to avoid use of SafeMath functions
 //But, if you want it...
 //import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
-//using SafeMath for uint; // 
+//using SafeMath for uint; //
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// NOTE: This contract is a challenge for https://membrane.trade 
+// NOTE: This contract is a challenge for https://membrane.trade
 /*
 Context:
 SushiSwap is a decentralized exchange (DEX) protocol based off Uniswap. 
@@ -39,31 +39,60 @@ Proposed solution:
 
  */
 
+interface SushiSwapPair {
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 _reserve0,
+            uint112 _reserve1,
+            uint32 _blockTimestampLast
+        );
+}
+
+interface SushiSwapV2Factory {
+    function getPair(address, address) external view returns (address);
+}
 
 interface SushiSwapRouter {
+    function factory() external view returns (address);
+
+    function quote(
+        uint256 amountA,
+        uint256 reserveA,
+        uint256 reserveB
+    ) external pure returns (uint256 amountB);
+
     function addLiquidity(
         address tokenA,
         address tokenB,
-        uint amountADesired,
-        uint amountBDesired,
-        uint amountAMin,
-        uint amountBMin,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
         address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB, uint liquidity);
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
     function removeLiquidity(
         address tokenA,
         address tokenB,
-        uint liquidity,
-        uint amountAMin,
-        uint amountBMin,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
         address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB);
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
 }
 
 /// @title Context
-/// @author OpenZeppelin ? 
+/// @author OpenZeppelin ?
 /// @notice This mini contract encapsulates the msg.sender property of a transaction.
 /// @dev This mini contract encapsulates the msg.sender property of a transaction, to ensure best code following and debug
 abstract contract Context {
@@ -73,14 +102,15 @@ abstract contract Context {
 }
 
 /**
-*@title Ownable
-*@author OpenZeppelin?
-*@notice This mini contract encapsulates all owner operations of the contract.
-*@dev This mini contract encapsulates all owner operations of the contract, to ensure best code following and debug.
-*I deleted all non-relevant code, like transfer ownership
-*/
+ *@title Ownable
+ *@author OpenZeppelin?
+ *@notice This mini contract encapsulates all owner operations of the contract.
+ *@dev This mini contract encapsulates all owner operations of the contract, to ensure best code following and debug.
+ *I deleted all non-relevant code, like transfer ownership
+ */
 abstract contract Ownable is Context {
     address internal _owner;
+
     /**
      * @dev Returns the address of the current owner.
      */
@@ -97,58 +127,143 @@ abstract contract Ownable is Context {
     }
 }
 
-
 /// @title SushiWallet
 /// @author Matías Araujo
 /// @notice Explain to an end user what this does
 /// @dev Explain to a developer any extra details
 
-contract SushiWallet is Ownable{
-
+contract SushiWallet is Ownable {
     string public constant name = "SushiWallet";
     address SushiRouterAddress; //Addr for SushiSwap router: https://dev.sushi.com/sushiswap/contracts ( 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506 )
 
-    event Rescue(address tokenAddress,uint256 amount);
+    event Rescue(address tokenAddress, uint256 amount);
 
     constructor(address sushiRouterAddr) {
         _owner = msg.sender;
         SushiRouterAddress = sushiRouterAddr;
-        
     }
 
-    function subscribeToPool( address token_A_addr, address token_B_addr, uint token_A_amount, uint token_B_amount) onlyOwner public returns (uint256) {
+    function subscribeToPool(
+        address token_A_addr,
+        address token_B_addr,
+        uint256 token_A_amount,
+        uint256 token_B_amount
+    ) public onlyOwner returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
+        // Order tokens to get normalization in the data treatment.
+        (address token0, address token1) = token_A_addr < token_B_addr
+            ? (token_A_addr, token_B_addr)
+            : (token_B_addr, token_A_addr);
+        (uint256 token0Amount, uint256 token1Amount) = token_A_addr < token_B_addr
+            ? (token_A_amount, token_B_amount)
+            : (token_B_amount, token_A_amount);
+
         //1st: approve tokens A for the desired amount. Why not check for allow first? Because i will approve only for the current tx.
-        if(!approveTokensForRouter(token_A_addr,token_A_amount)){
+        if (!approveTokensForRouter(token0, token0Amount)) {
             //something has failed
             console.log("Error at Approve A tokens.");
             revert("Cannot Approve A Tokens");
         }
-        if(!approveTokensForRouter(token_B_addr,token_B_amount)){
+        if (!approveTokensForRouter(token1, token1Amount)) {
             //something has failed
             console.log("Error at Approve B tokens.");
             revert("Cannot Approve B Tokens");
         }
+        //2nd: Calculate parity of tokens, if exists. If it does not exist, return 0. (I think that you can define your own LP with your own ratio, and SushiSwap
+        // creates the LP by yourself, but i'm not sure and this is money.)
+        uint256 amountBMin = calculateAmountOfToken1(token0, token1, token0Amount);
+        if (amountBMin == 0) {
+            console.log("The pair does not exist.");
+            revert("The pair does not exist.");
+        }
+        console.log(amountBMin);
+        if (amountBMin > token1Amount) {
+            //it means that we didn't provide enought funds of the second token
+            console.log("Not enought funds for the second token.");
+            revert("Not enought funds for the second token.");
+        }
 
-        //Call SushiSwap to provide liquidity
+        //3rd: Try to call SushiSwap to provide liquidity, tolerate only 0,5 of slippage, and only 30min for timeout
+        (amountA,amountB,liquidity) = provideLiquidity(token0,token1,token0Amount,token1Amount);
     }
 
     /// @notice Explain to an end user what this does
     /// @dev Explain to a developer any extra details
     /// @param erc20TokenAddress a parameter just like in doxygen (must be followed by parameter name)
     /// @return success the return variables of a contract’s function state variable
-    function approveTokensForRouter(address erc20TokenAddress, uint256 amountApprovedToSpend) onlyOwner public returns (bool success) {
+    function approveTokensForRouter(address erc20TokenAddress, uint256 amountApprovedToSpend)
+        internal
+        onlyOwner
+        returns (bool success)
+    {
         //It should be something like:
-        return IERC20(erc20TokenAddress).approve(SushiRouterAddress,amountApprovedToSpend);
+        return IERC20(erc20TokenAddress).approve(SushiRouterAddress, amountApprovedToSpend);
     }
 
-    function approveTokensForMasterChef(address erc20TokenAddress) onlyOwner public returns (uint256) {}
+    function approveTokensForMasterChef(address erc20TokenAddress) internal onlyOwner returns (uint256) {}
 
-    function provideLiquidity(address erc20TokenAddress_1 , uint256 amount_1 ,address erc20TokenAddress_2 , uint256 amount_2) onlyOwner public returns (uint256) {
+    /// @notice provideLiquidity
+    /// @dev Call Pool, compute 0,5% of slippage, and the timeout. Returns with the values of the pool
+    /// @param tokenA First token to provide liquidity
+    /// @param tokenB Second token to provide liquidity
+    /// @param amountADesired First Token Amount to provide
+    /// @param amountBDesired Second Token Amount to provide
 
+    /// @return amountA the amount of First Token added to the pool.
+    /// @return amountB the amount of First Token added to the pool.
+    /// @return liquidity the percentage of the pool.
+
+    function provideLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired
+    )
+        internal
+        onlyOwner
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        )
+    {
+        uint256 amountAMin = 995*amountADesired / 1000;
+        uint256 amountBMin = 995*amountBDesired / 1000;
+        uint256 deadline = block.timestamp + 30 minutes;
+        return
+            SushiSwapRouter(SushiRouterAddress).addLiquidity(
+                tokenA,
+                tokenB,
+                amountADesired,
+                amountBDesired,
+                amountAMin,
+                amountBMin,
+                address(this),
+                deadline
+            );
     }
 
+    //Encapsulate the calculation if pair exists.
+    function checkIfPairExists(address tokenA, address tokenB) public view onlyOwner returns (address pair) {
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        pair = SushiSwapV2Factory(SushiSwapRouter(SushiRouterAddress).factory()).getPair(token0, token1);
+    }
 
-    function rescueTokens(address _token) onlyOwner external {
+    //encapsulate the calculation of x*y=k if pair exists.
+    function calculateAmountOfToken1(
+        address token0,
+        address token1,
+        uint256 amount0
+    ) public view onlyOwner returns (uint256 token1Amount) {
+        //1st: locate pair, if exists. If it not exists, return 0.
+        address pair = checkIfPairExists(token0, token1);
+        if (pair == address(0)) {
+            return 0;
+        }
+        (uint256 reserve0, uint256 reserve1, uint256 lastBlockTimestamp) = SushiSwapPair(pair).getReserves();
+        token1Amount = SushiSwapRouter(SushiRouterAddress).quote(amount0, reserve0, reserve1);
+    }
+
+    function rescueTokens(address _token) external onlyOwner {
         uint256 amount = IERC20(_token).balanceOf(address(this));
         IERC20(_token).transfer(_msgSender(), amount);
         emit Rescue(_token, amount);
